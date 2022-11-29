4a8071da11579d7cb16b0a1c5d757439a3566dbc
fahim-ash <ashhabfahim@gmail.com>
2022-11-29 18:32:23 +0600

    'stable version'

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
